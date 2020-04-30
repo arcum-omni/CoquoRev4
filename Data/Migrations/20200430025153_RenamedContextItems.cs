70ee50e08987f99923559baab6e61f89ec98ab36
arcum-omni <travis.l.eiler@gmail.com>
2020-04-29 19:54:39 -0700

    Added MIgration, Identity functioning

---
+++ Data/Migrations/20200430025153_RenamedContextItems.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoquoRev4.Data.Migrations
+{
+    public partial class RenamedContextItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
