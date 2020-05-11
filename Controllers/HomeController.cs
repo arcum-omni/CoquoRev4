de1ab8afb131b5f786b5073dd7fd433eb30043e5
Joseph Ortiz <joeprogrammer88@users.noreply.github.com>
2020-05-11 13:57:12 -0700

    In class

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,21 +6,35 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CoquoRev4.Models;
+using CoquoRev4.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoquoRev4.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly KitchenDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+            KitchenDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int id)
         {
-            return View();
+            // Single dish with ingredients
+            Dish singleDish = _context.Dishes
+                    .Where(d => d.DishID == 2)
+                    .Include(d => d.Cooks)          // Include Cooks in Dishes
+                    .ThenInclude(c => c.Ingredient) // Include the Ingredients for each Cook
+                    .SingleOrDefault();
+
+            
+
+            return View(singleDish);
         }
 
         public IActionResult Privacy()
